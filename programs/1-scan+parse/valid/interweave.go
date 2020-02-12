1263a450d76712fa35bbef1970dcfd522c3cc019
sophiawho <sophiahuyt@gmail.com>
2020-02-12 12:29:32 -0500

    Update README.md and indent scanner

---
+++ programs/1-scan+parse/valid/interweave.go
@@ -0,0 +1,18 @@
+package main
+
+var x int // type only
+var y = 42 // expression only
+var z = 1
+
+func main() {
+	var x int // type only
+	var y = 42 // expression only
+
+	y++;
+
+	var z int = 1;
+
+	var z blah = 2;
+}
+
+var zz = 2
