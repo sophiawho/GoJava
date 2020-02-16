d264244b9746a05caa8f7e0da21962e3ee85e41b
Matthew Lesko-Krleza <mlesko1996@gmail.com>
2020-02-16 12:32:56 -0500

    Test for switch statements and more function declarations
    Currently parser fails at both

--- programs/1-scan+parse/valid/function-decl1.go
+++ programs/1-scan+parse/valid/function-decl1.go
@@ -1,10 +1,96 @@
 package main
 
-func f(a int, int b, int c) {
-	return
-}
+// Without params
 
-// short form
-func f(a, b int, c string, d int) string {
-	return c
-}
+//// With returns
+func f() t_type {return a}
+
+func f() []t_type {return a}
+
+func f() [3]t_type {return a}
+
+//// Without returns
+func f() {}
+
+
+// With params
+
+//// With returns
+////// short form
+func f(a, b int, c string, d, e t_type) t_type {return f}
+
+func f(a, b int, c string, d, e t_type) []t_type {return f}
+
+func f(a, b int, c string, d, e t_type) [3]t_type {return f}
+
+func f(a, b []t_type) t_type {return f}
+
+func f(a, b []t_type) []t_type {return f}
+
+func f(a, b []t_type) [3]t_type {return f}
+
+func f(a, b [3]t_type) t_type {return f}
+
+func f(a, b [3]t_type) []t_type {return f}
+
+func f(a, b [3]t_type) [3]t_type {return f}
+
+////// long form
+func f(b int, c string, e t_type) t_type {return f}
+
+func f(b int, c string, e t_type) []t_type {return f}
+
+func f(b int, c string, e t_type) [3]t_type {return f}
+
+func f(b []t_type) t_type {return f}
+
+func f(b []t_type) []t_type {return f}
+
+func f(b []t_type) [3]t_type {return f}
+
+func f(b [3]t_type) t_type {return f}
+
+func f(b [3]t_type) []t_type {return f}
+
+func f(b [3]t_type) [3]t_type {return f}
+
+//// Without returns
+////// short form
+func f(a, b int, c string, d, e t_type) {}
+
+func f(a, b int, c string, d, e t_type) {}
+
+func f(a, b int, c string, d, e t_type) {}
+
+func f(a, b []t_type) {}
+
+func f(a, b []t_type) {}
+
+func f(a, b []t_type) {}
+
+func f(a, b [3]t_type) {}
+
+func f(a, b [3]t_type) {}
+
+func f(a, b [3]t_type) {}
+
+////// long form
+func f(b int, c string, e t_type) {}
+
+func f(b int, c string, e t_type) {}
+
+func f(b int, c string, e t_type) {}
+
+func f(b []t_type) {}
+
+func f(b []t_type) {}
+
+func f(b []t_type) {}
+
+func f(b [3]t_type) {}
+
+func f(b [3]t_type) {}
+
+func f(b [3]t_type) {}
+
+
---
+++ programs/1-scan+parse/valid/switch1.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	var a = 1
+
+	switch a {
+	case 0, 1, 2, 3: s1()
+	case 4, 5, 6, 7: s2()
+	}
+
+}
