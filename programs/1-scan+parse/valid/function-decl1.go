cabc143e4b031a5e363b177eb896da0a353f0af7
Matthew Lesko-Krleza <mlesko1996@gmail.com>
2020-02-19 12:11:05 -0500

    Fix valid program
    Same line return statements require a semicolon in golite!

--- programs/1-scan+parse/valid/function-decl1.go
+++ programs/1-scan+parse/valid/function-decl1.go
@@ -3,11 +3,11 @@ package main
 // Without params
 
 //// With returns
-func f() t_type {return a}
+func f() t_type {return a;}
 
-func f() []t_type {return a}
+func f() []t_type {return a;}
 
-func f() [3]t_type {return a}
+func f() [3]t_type {return a;}
 
 //// Without returns
 func f() {}
@@ -17,42 +17,42 @@ func f() {}
 
 //// With returns
 ////// short form
-func f(a, b int, c string, d, e t_type) t_type {return f}
+func f(a, b int, c string, d, e t_type) t_type {return f;}
 
-func f(a, b int, c string, d, e t_type) []t_type {return f}
+func f(a, b int, c string, d, e t_type) []t_type {return f;}
 
-func f(a, b int, c string, d, e t_type) [3]t_type {return f}
+func f(a, b int, c string, d, e t_type) [3]t_type {return f;}
 
-func f(a, b []t_type) t_type {return f}
+func f(a, b []t_type) t_type {return f;}
 
-func f(a, b []t_type) []t_type {return f}
+func f(a, b []t_type) []t_type {return f;}
 
-func f(a, b []t_type) [3]t_type {return f}
+func f(a, b []t_type) [3]t_type {return f;}
 
-func f(a, b [3]t_type) t_type {return f}
+func f(a, b [3]t_type) t_type {return f;}
 
-func f(a, b [3]t_type) []t_type {return f}
+func f(a, b [3]t_type) []t_type {return f;}
 
-func f(a, b [3]t_type) [3]t_type {return f}
+func f(a, b [3]t_type) [3]t_type {return f;}
 
 ////// long form
-func f(b int, c string, e t_type) t_type {return f}
+func f(b int, c string, e t_type) t_type {return f;}
 
-func f(b int, c string, e t_type) []t_type {return f}
+func f(b int, c string, e t_type) []t_type {return f;}
 
-func f(b int, c string, e t_type) [3]t_type {return f}
+func f(b int, c string, e t_type) [3]t_type {return f;}
 
-func f(b []t_type) t_type {return f}
+func f(b []t_type) t_type {return f;}
 
-func f(b []t_type) []t_type {return f}
+func f(b []t_type) []t_type {return f;}
 
-func f(b []t_type) [3]t_type {return f}
+func f(b []t_type) [3]t_type {return f;}
 
-func f(b [3]t_type) t_type {return f}
+func f(b [3]t_type) t_type {return f;}
 
-func f(b [3]t_type) []t_type {return f}
+func f(b [3]t_type) []t_type {return f;}
 
-func f(b [3]t_type) [3]t_type {return f}
+func f(b [3]t_type) [3]t_type {return f;}
 
 //// Without returns
 ////// short form
