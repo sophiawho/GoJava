faf094b68515a3efc51d21d5f8503524196f4a19
sophiawho <sophiahuyt@gmail.com>
2020-03-06 14:59:47 -0500

    Add milestone1 test programs

---
+++ programs-solution/1-scan+parse/invalid/8-12-for_statements/forstmt3_7.go
@@ -0,0 +1,8 @@
+package test
+
+func main() {
+    for i := 0; i < 10; print(i) {
+
+    }
+}
+ 
---
+++ programs/1-scan+parse/invalid/midterm.go
@@ -0,0 +1,9 @@
+package main
+
+var a int = 3
+
+func main() {
+	var a, b int
+	var int float64
+	a = 0xff
+}
