a3e24da9a8b8270bc061e19d67df6cab547886f1
Matthew Lesko-Krleza <mlesko1996@gmail.com>
2020-03-07 14:59:04 -0500

    Create tests
    1 invalid: use builtin keyword in var decl. 1 valid: index array returned from function call

---
+++ programs/1-scan+parse/invalid/builtin1.go
@@ -0,0 +1,6 @@
+// can't use the print builtin in variable declaration
+package main
+
+func main() {
+    var print = 1
+}
---
+++ programs/2-typecheck/valid/assign1.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	f1()[1] = 2
+}
+
+
+func f1() []int {
+	var a []int
+	return a
+}
