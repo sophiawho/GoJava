24fa084ad4c0923bdda6b6270f9a72698db557fc
sophiawho <sophiahuyt@gmail.com>
2020-03-12 23:03:48 -0400

    Setup milestone 2

---
+++ src/typecheck/typecheck.c
@@ -0,0 +1,7 @@
+#include "typecheck.h"
+#include "../ast/tree.h"
+#include "../ast/stmt.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
---
+++ src/typecheck/typecheck.h
@@ -0,0 +1,7 @@
+#ifndef TYPECHECK_H
+#define TYPECHECK_H
+
+#include "../ast/tree.h"
+#include "../ast/stmt.h"
+
+#endif /* !TYPECHECK_H */
