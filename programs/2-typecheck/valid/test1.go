48920e94fb198d0214ab5487afe8161da4335dfc
Matthew Lesko-Krleza <mlesko1996@gmail.com>
2020-03-14 18:45:35 -0400

    Fix test

--- programs/2-typecheck/valid/test1.go
+++ programs/2-typecheck/valid/test1.go
@@ -1,24 +1,5 @@
 package main
 
-func main() {
-	var a = 1
-	var b = 2
-	var _ int = 1
-	print(a)
-	println(b)
-	f1()
-	f2()
-	f3()
-	f4()
-	var c, d, _ int = 1, 2, 3
-	for i := 0; i < 5; i++ {
-		continue
-	}
-	for i := 0; i < 5; i++ {
-		break
-	}
-}
-
 func _() int {return 0;}
 func _() {return;}
 func f1() []int {
@@ -43,3 +24,23 @@ func f4() p {
 	var a p
 	return a
 }
+
+func main() {
+	var a = 1
+	var b = 2
+	var _ int = 1
+	print(a)
+	println(b)
+	f1()
+	f2()
+	f3()
+	f4()
+	var c, d, _ int = 1, 2, 3
+	for i := 0; i < 5; i++ {
+		continue
+	}
+	for i := 0; i < 5; i++ {
+		break
+	}
+}
+
