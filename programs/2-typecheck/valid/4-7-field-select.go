2dfa4bf0535c5d2560636a93a959540ee56d8eb0
sophiawho <sophiahuyt@gmail.com>
2020-03-16 12:07:12 -0400

    Comment out tests for unimplemented typecheck expressions

--- programs/2-typecheck/valid/4-7-field-select.go
+++ programs/2-typecheck/valid/4-7-field-select.go
@@ -13,10 +13,10 @@ func foo() {
 	type structType2 structType
 	var structElem structType
 
-	var e, e2 int = structElem.a, structElem.a
-	var f float64 = structElem.b
-	var g rune = structElem.c
-	var h string = structElem.d
-	var i [5]int = structElem.e
-	var j int = structElem.e[2]
+	// var e, e2 int = structElem.a, structElem.a
+	// var f float64 = structElem.b
+	// var g rune = structElem.c
+	// var h string = structElem.d
+	// var i [5]int = structElem.e
+	// var j int = structElem.e[2]
 }
