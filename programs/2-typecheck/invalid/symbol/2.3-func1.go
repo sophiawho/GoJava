ffb90701f62f40c7d91abb356eece87d28bac4cd
sophiawho <sophiahuyt@gmail.com>
2020-03-16 21:22:49 -0400

    Add invalid function declaration tests

---
+++ programs/2-typecheck/invalid/symbol/2.3-func1.go
@@ -0,0 +1,6 @@
+// redeclares formal parameter a
+package test
+
+func foo(a int, b int, c rune) {
+	var a float64
+}
---
+++ programs/2-typecheck/invalid/symbol/2.3-func2.go
@@ -0,0 +1,6 @@
+// two functions have the same name
+package test
+
+func foo(a int, b int, a rune) {
+	print(a, b)
+}
