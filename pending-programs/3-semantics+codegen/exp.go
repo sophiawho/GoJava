e7b136e64156b0375e513dcaace2dfa41051c3cd
sophiawho <sophiahuyt@gmail.com>
2020-04-03 20:08:10 -0400

    Add tests for binary expressions

---
+++ pending-programs/3-semantics+codegen/exp.go
@@ -0,0 +1,19 @@
+//~appleApple
+//~false
+//~8 -2
+//~
+
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+	var a string = "apple"
+	var b string = "Apple"
+	println(a+b)
+	println(a<b)
+
+	println(3+5, 3-5)
+}
