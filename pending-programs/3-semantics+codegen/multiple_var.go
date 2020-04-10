92bb7e35a73d1ccc21830bca24c666d5dd82d845
sophiawho <sophiahuyt@gmail.com>
2020-04-10 15:35:27 -0400

    Write new tests and move arrayequality.go from pending

---
+++ pending-programs/3-semantics+codegen/multiple_var.go
@@ -0,0 +1,10 @@
+//~1 2 0 0
+//~
+
+package test
+
+func main() {
+	var a, b = 1, 2
+	var c, d int
+	println(a, b, c, d)
+}
--- programs/3-semantics+codegen/valid/arrayequality.go
+++ programs/3-semantics+codegen/valid/arrayequality.go
@@ -5,7 +5,8 @@
 package main
 
 func main() {
-	var a, b [5] int
+	var a [5] int
+	var b [5] int
 	println(a==b) // True
 	
 	b[0]=1
