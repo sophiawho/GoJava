24d8a80f4dbfb089d164a9047f3b86f97f1b8a94
sophiawho <sophiahuyt@gmail.com>
2020-04-14 18:30:17 -0400

    Move contents of passing array equality test

--- programs/3-semantics+codegen/valid/array1.go
+++ programs/3-semantics+codegen/valid/array1.go
@@ -1,9 +1,27 @@
 //~true
 //~false
+//0
+//1
+//0
+//0
+//~true
+//~false
 //~1 2 0 0 1
 //~
 
 package test
+
+func init() {
+	var a, b [2] int 
+	println(a==b)
+	b[1] = 1
+	println(a==b)
+
+	println(b[0])
+	println(b[1])
+	println(a[0])
+	println(a[1])
+}
 
 func init() {
 	var a [5] int
