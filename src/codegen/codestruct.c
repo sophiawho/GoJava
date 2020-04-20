dd83e483bb3925558180770a7950bb8a3e3f3c57
sophiawho <sophiahuyt@gmail.com>
2020-04-20 18:47:24 -0400

    Remove comments

--- src/codegen/codestruct.c
+++ src/codegen/codestruct.c
@@ -1,18 +1,3 @@
-/**
-
-TODO
-
-Traverse entire program
-Print out structs from type definitions, var definitions, untagged functions as: `__golite__struct__$structNumber`
-- Helper functions
-- Eg: equality
-
-How to tell if two untagged structs are equal? ie: Struct definition equality
-
-Struct scoping?
-
-*/
-
 #include "codestruct.h"
 #include "../ast/tree.h"
 #include "../ast/stmt.h"
